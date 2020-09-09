c8ddc29e5760d1e9c01d89f262304906a8f15ea2
mithun2534 <mithun2534@gmail.com>
2020-09-08 23:12:17 -0400

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Net.Client;
+using GrpcServer;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var input = new HelloRequest { Name = "Mithun " };
+            var input2 = new TestSvc { Id = "Sahithi" };
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            
+
+            var reply = await client.SayHelloAsync(input);
+            var reply2 = await client.SayHello2Async(input2);
+
+            Console.WriteLine(reply.Message);
+            Console.WriteLine(reply2.Message);
+            Console.ReadLine();
+
+        }
+    }
+}
